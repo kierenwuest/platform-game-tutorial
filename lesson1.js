d69419270efe9db890dfff0502fa82fa32ea6f89
Kieren Wuest <kierenwuest@gmail.com>
2022-09-14 02:41:06 +0930

    finsihed

--- lesson1.js
+++ lesson1.js
@@ -1,20 +1,20 @@
 /** @type {HTMLCanvasElement} */ //This give VSCode Intellisense the canvas context methods
 
-let playerAnimState = 'idle';
-const dropdown = document.getElementById('animations');
-dropdown.addEventListener('change',function(event){
-    playerAnimState = event.target.value;
-})
-const canvas = document.getElementById('first');
-const ctx = canvas.getContext('2d');
+let playerAnimState = "idle";
+const dropdown = document.getElementById("animations");
+dropdown.addEventListener("change", function (event) {
+  playerAnimState = event.target.value;
+});
+const canvas = document.getElementById("first");
+const ctx = canvas.getContext("2d");
 
 // console.log(ctx); // Looks at all the particulars of canvas on the page
 
-const ctxWidth = canvas.width = 600; // Without this the default canvas xy is 300 x 150 for some reason
-const ctxHeight = canvas.height = 600; 
+const ctxWidth = (canvas.width = 600); // Without this the default canvas xy is 300 x 150 for some reason
+const ctxHeight = (canvas.height = 600);
 
 const charImage = new Image();
-charImage.src = 'images/Ranger_288x128_SpriteSheet.png' // https://chierit.itch.io/elementals-leaf-ranger
+charImage.src = "images/Ranger_288x128_SpriteSheet.png"; // https://chierit.itch.io/elementals-leaf-ranger
 // idle
 // run
 // jumpUp
@@ -40,7 +40,7 @@ const spriteHeight = 128;
 let gameframe = 0;
 const staggerFrames = 4.5; // lower number is faster controls animationLoop speed
 
-// trying to achieve an array like this for a map of the xy crop frames of each animation set 
+// trying to achieve an array like this for a map of the xy crop frames of each animation set
 // spriteAnimations = [
 //     "idle" = {
 //         location: [
@@ -65,110 +65,112 @@ const staggerFrames = 4.5; // lower number is faster controls animationLoop spee
 
 const spriteAnimations = []; // array for sprites line frames(columns) and rows
 
-const animationStates = [  // simple map for the sprite sheet 
-    {
-        name: 'idle',
-        frames: 12,
-    }, 
+const animationStates = [
+  // simple map for the sprite sheet
+  {
+    name: "idle",
+    frames: 12,
+  },
 
-    {
-        name: 'run',
-        frames: 10,
-    },
+  {
+    name: "run",
+    frames: 10,
+  },
 
-    {
-        name: 'jumpUp',
-        frames: 22,
-    },
+  {
+    name: "jumpUp",
+    frames: 22,
+  },
 
-    {
-        name: 'jumpUpLoop',
-        frames: 3,
-    },
+  {
+    name: "jumpUpLoop",
+    frames: 3,
+  },
 
-    {
-        name: 'jumpDown',
-        frames: 22,
-    },
+  {
+    name: "jumpDown",
+    frames: 22,
+  },
 
-    {
-        name: 'jumpDownLoop',
-        frames: 3,
-    },
+  {
+    name: "jumpDownLoop",
+    frames: 3,
+  },
 
-    {
-        name: 'jumpAtk',
-        frames: 22,
-    },
+  {
+    name: "jumpAtk",
+    frames: 22,
+  },
 
-    {
-        name: 'jumpAtkAction',
-        frames: 10,
-    },
+  {
+    name: "jumpAtkAction",
+    frames: 10,
+  },
 
-    {
-        name: 'roll',
-        frames: 8,
-    },
+  {
+    name: "roll",
+    frames: 8,
+  },
 
-    {
-        name: 'slide',
-        frames: 13,
-    },
+  {
+    name: "slide",
+    frames: 13,
+  },
 
-    {
-        name: 'arrowStab',
-        frames: 10,
-    },
+  {
+    name: "arrowStab",
+    frames: 10,
+  },
 
-    {
-        name: 'shoot',
-        frames: 15,
-    },
+  {
+    name: "shoot",
+    frames: 15,
+  },
 
-    {
-        name: 'shootUpMany',
-        frames: 12,
-    },
+  {
+    name: "shootUpMany",
+    frames: 12,
+  },
 
-    {
-        name: 'shootPower',
-        frames: 17,
-    },
+  {
+    name: "shootPower",
+    frames: 17,
+  },
 
-    {
-        name: 'blockRoot',
-        frames: 19,
-    },
+  {
+    name: "blockRoot",
+    frames: 19,
+  },
 
-    {
-        name: 'takeHit',
-        frames: 6,
-    },
+  {
+    name: "takeHit",
+    frames: 6,
+  },
 
-    {
-        name: 'death',
-        frames: 19,
-    }
-
+  {
+    name: "death",
+    frames: 19,
+  },
 ];
 
 animationStates.forEach((state, index) => {
-    let frames = { /// setting let frames to an object {} this should be the number of animation frames in the row
-        location: [],
-    }
-    for (let j = 0; j < state.frames; j++) { // put the index of the animate state into array
-        let positionX = j * spriteWidth; // for each for loop adding the 288 to the next array item - this is calculating the x's for the crop 
-        let positionY = index * spriteHeight; // for each index (the state) multiplying the height 128 for the row - this is calculating the y's for the crop    
-        frames.location.push({x:positionX, y:positionY}); // the push method inserts the for loop variable calculations and pushes them in a litte co-ord object {} into the location array
-    }
-    spriteAnimations[state.name] = frames; //this is creating a new key value in the blank array (up top) to look like the sprite animations structure as designed
+  let frames = {
+    /// setting let frames to an object {} this should be the number of animation frames in the row
+    location: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    // put the index of the animate state into array
+    let positionX = j * spriteWidth; // for each for loop adding the 288 to the next array item - this is calculating the x's for the crop
+    let positionY = index * spriteHeight; // for each index (the state) multiplying the height 128 for the row - this is calculating the y's for the crop
+    frames.location.push({ x: positionX, y: positionY }); // the push method inserts the for loop variable calculations and pushes them in a litte co-ord object {} into the location array
+  }
+  spriteAnimations[state.name] = frames; //this is creating a new key value in the blank array (up top) to look like the sprite animations structure as designed
 });
 
 console.log(spriteAnimations); //! this tests the result
 // console log result
 // [idle: {…}, run: {…}]
-// idle: 
+// idle:
 // location: Array(12)
 // 0 : {x: 0, y: 0}
 // 1 : {x: 288, y: 0}
@@ -196,38 +198,49 @@ console.log(spriteAnimations); //! this tests the result
 // 7 : {x: 2016, y: 128}
 // 8 : {x: 2304, y: 128}
 // 9 : {x: 2592, y: 128}
-// length:  10 
-//! notice the calculated x and y postions. spriteAnimations ARRAY[], which was empty, has a 'state' name OBJECT{}, and location ARRAY[] containing little xy OBJECTS{} 
+// length:  10
+//! notice the calculated x and y postions. spriteAnimations ARRAY[], which was empty, has a 'state' name OBJECT{}, and location ARRAY[] containing little xy OBJECTS{}
 //! The animationStates.forEach method did this, from the animationStates ARRAY [] map with the name and frames defined in it above in the let vars. - setting the rest of the states in this array will then calculate all the animation frame sets as they are defined in the let ARRAY
 
 // ------------------ Animation Canvas Loop ------------------ //
 
-function animationLoop () {
-    ctx.clearRect(0,0,ctxWidth,ctxHeight);
-    ctx.fillStyle = 'lightgrey';
-    ctx.fillRect(0,0, 288 , 128)
-    let position = Math.floor(gameframe/staggerFrames) % spriteAnimations[playerAnimState].location.length; //gameframe position - only integers and modulous to make end frame 1?  https://youtu.be/GFO_txvwK_c?t=1443
-    let frameX = spriteWidth * position;
-    let frameY = spriteAnimations[playerAnimState].location[position].y;
-    // ctx.drawImage(imagesrc,dx,dy, dw , dh ); // 5 arg drawImage is used to draw images
-    // ctx.drawImage(imagesrc,sx,sy, sw , sh ,dx,dy, dw , dh ); // 9 arg drawImage is used to position (d Destination/Draw) and cut/crop with the (s Source) images - sx sy for frame position
-    ctx.drawImage(charImage,frameX,frameY, spriteWidth , spriteHeight ,0,0, spriteWidth , spriteHeight);
-    //ctx.strokeRect(0,0, 288 , 128);
-    // if (gameframe % staggerFrames == 0) { // inserts gameframes - primative way
-    // if (frameX < 11) frameX++; // animates the sprite frame if (up to end frame) incrments by one
-    // else frameX = 0;} // reset to 0 at end of frames
-    gameframe++; // inserts gameframes
-    requestAnimationFrame(animationLoop);
+function animationLoop() {
+  ctx.clearRect(0, 0, ctxWidth, ctxHeight);
+  ctx.fillStyle = "lightgrey";
+  ctx.fillRect(0, 0, 288, 128);
+  let position =
+    Math.floor(gameframe / staggerFrames) %
+    spriteAnimations[playerAnimState].location.length; //gameframe position - only integers and modulous to make end frame 1?  https://youtu.be/GFO_txvwK_c?t=1443
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations[playerAnimState].location[position].y;
+  // ctx.drawImage(imagesrc,dx,dy, dw , dh ); // 5 arg drawImage is used to draw images
+  // ctx.drawImage(imagesrc,sx,sy, sw , sh ,dx,dy, dw , dh ); // 9 arg drawImage is used to position (d Destination/Draw) and cut/crop with the (s Source) images - sx sy for frame position
+  ctx.drawImage(
+    charImage,
+    frameX,
+    frameY,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
+  //ctx.strokeRect(0,0, 288 , 128);
+  // if (gameframe % staggerFrames == 0) { // inserts gameframes - primative way
+  // if (frameX < 11) frameX++; // animates the sprite frame if (up to end frame) incrments by one
+  // else frameX = 0;} // reset to 0 at end of frames
+  gameframe++; // inserts gameframes
+  requestAnimationFrame(animationLoop);
 }
 
 animationLoop();
 
-
 // idle
-// run 
+// run
 // jump
-// j_up 
-// j_down 
+// j_up
+// j_down
 // air_atk_sample4
 // air_atk
 // roll
--- lesson2.js
+++ lesson2.js
@@ -4,138 +4,178 @@
 //background images https://edermunizz.itch.io/free-pixel-art-forest
 // 928 w x 793 h
 
-const canvas = document.getElementById('second');
-const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 928;
-const CANVAS_HEIGHT = canvas.height = 793;
-
+const canvas = document.getElementById("second");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 928);
+const CANVAS_HEIGHT = (canvas.height = 793);
 
 const backgroundLayer0 = new Image(); //* Ground Front
-backgroundLayer0.src = '/images/Layer00_9.png';
+backgroundLayer0.src = "/images/Layer00_9.png";
 
 const backgroundLayer1 = new Image(); //* Ground Back
-backgroundLayer1.src = '/images/Layer01_8.png';
+backgroundLayer1.src = "/images/Layer01_8.png";
 
 const backgroundLayer2 = new Image(); //* Tree Tops
-backgroundLayer2.src = '/images/Layer02_7.png';
+backgroundLayer2.src = "/images/Layer02_7.png";
 
 const backgroundLayer3 = new Image(); //* Front Trunks
-backgroundLayer3.src = '/images/Layer03_6.png';
+backgroundLayer3.src = "/images/Layer03_6.png";
 
 const backgroundLayer4 = new Image(); //* Front Lights
-backgroundLayer4.src = '/images/Layer04_Lights.png';
+backgroundLayer4.src = "/images/Layer04_Lights.png";
 
-const backgroundLayer5 = new Image(); //* Front Middle Trunks  
-backgroundLayer5.src = '/images/Layer05_5.png';
+const backgroundLayer5 = new Image(); //* Front Middle Trunks
+backgroundLayer5.src = "/images/Layer05_5.png";
 
 const backgroundLayer6 = new Image(); //* Back Middle Trunks
-backgroundLayer6.src = '/images/Layer06_4.png';
+backgroundLayer6.src = "/images/Layer06_4.png";
 
 const backgroundLayer7 = new Image(); //* Back Lights
-backgroundLayer7.src = '/images/Layer07_Lights.png';
+backgroundLayer7.src = "/images/Layer07_Lights.png";
 
 const backgroundLayer8 = new Image(); //* Back Trunks
-backgroundLayer8.src = '/images/Layer08_3.png';
+backgroundLayer8.src = "/images/Layer08_3.png";
 
 const backgroundLayer9 = new Image(); //* Fog Trunks
-backgroundLayer9.src = '/images/Layer09_2.png';
+backgroundLayer9.src = "/images/Layer09_2.png";
 
 const backgroundLayer10 = new Image(); //* Light Blue Fill
-backgroundLayer10.src = '/images/Layer10_1.png';
+backgroundLayer10.src = "/images/Layer10_1.png";
 
 const backgroundLayer11 = new Image(); //* 3 Tone Grad Fill
-backgroundLayer11.src = '/images/Layer11_0.png';
+backgroundLayer11.src = "/images/Layer11_0.png";
 
-let globalGameSpeed = 2;
+let globalGameSpeed = 3;
 
-class Layer { // classes start with capital and are like a blueprint, for similar qualites with different values. Require a mandatory construtor method()
-     // in a class the constructor Method is a function attached to an Object. 
-    //When the class is called it will assign properties to a new blank object based on the blueprint in the constructor 
-    //runs once per Object everytme the class is called with the 'new' keyword
-    constructor(image, speedMod , yOffset ) {
-        this.x = 0;
-        this.y = 0;
-        this.yOffset = yOffset; // used to adjust layer height: positive is down, negative is up
-        this.width = 928;
-        this.height = 793;
-        this.image = image; // this is getting passed from the constructor (argument)
-        this.speedMod = speedMod; // as above
-        this.speed = globalGameSpeed * this.speedMod; // this ties it all to the global globalGameSpeed var
+const slider = document.getElementById('slider');
+slider.value = globalGameSpeed;
+const showScrollSpeed = document.getElementById('showScrollSpeed');
+showScrollSpeed.innerHTML = globalGameSpeed;
+slider.addEventListener('change',function(e){
+    globalGameSpeed = e.target.value;
+    showScrollSpeed.innerHTML = e.target.value;
+});
+
+
+
+class Layer {
+  // classes start with capital and are like a blueprint, for similar qualites with different values. Require a mandatory construtor method()
+  // in a class the constructor Method is a function attached to an Object.
+  //When the class is called it will assign properties to a new blank object based on the blueprint in the constructor
+  //runs once per Object everytme the class is called with the 'new' keyword
+  constructor(image, speedMod, yOffset) {
+    this.x = 0;
+    this.y = 0;
+    this.yOffset = yOffset; // used to adjust layer height: positive is down, negative is up
+    this.width = 928;
+    this.height = 793;
+    this.image = image; // this is getting passed from the constructor (argument)
+    this.speedMod = speedMod; // as above
+    this.speed = globalGameSpeed * this.speedMod; // this ties it all to the global globalGameSpeed var
+  }
+  update() {
+    // and update method in the class which updates values
+    this.speed = globalGameSpeed * this.speedMod;
+    if (this.x <= -this.width) {
+      // this is detecting the edge and reset to 0
+      this.x = 0;
     }
-    update(){ // and update method in the class which updates values
-        this.speed = globalGameSpeed * this.speedMod; 
-        if (this.x <= -this.width) { // this is detecting the edge and reset to 0
-            this.x = 0;
-        }
-        this.x = this.x - this.speed; // T is moving the image to the left. + will move to the right but the offest image needs to be on the otheside
-    }
-    draw(){ // draws stuff on the canvas
-        ctx.drawImage(this.image, this.x, this.y + this.yOffset , this.width+2 , this.height)
-        ctx.drawImage(this.image, this.x + this.width , + this.yOffset , this.width , this.height) // this replaces the paris of draw image below
-    }
+    this.x = this.x - this.speed; // T is moving the image to the left. + will move to the right but the offest image needs to be on the otheside
+  }
+  draw() {
+    // draws stuff on the canvas
+    ctx.drawImage(
+      this.image,
+      this.x,
+      this.y + this.yOffset,
+      this.width + 2,
+      this.height
+    );
+    ctx.drawImage(
+      this.image,
+      this.x + this.width,
+      +this.yOffset,
+      this.width,
+      this.height
+    ); // this replaces the paris of draw image below
+  }
 }
 
 // These will set the layer relative speeds by creating new Objects from the class to be drawn in the animation loop
 
-const layer0 = new Layer ( backgroundLayer0 , 1 , 15 ); //* Ground Front
-const layer1 = new Layer ( backgroundLayer1 , 0.85 , 0 ); //* Ground Back
-const layer2 = new Layer ( backgroundLayer2 , 0.8 , 0 ); //* Tree Tops
-const layer3 = new Layer ( backgroundLayer3 , 0.7 , 0 ); //* Front Trunks
-const layer4 = new Layer ( backgroundLayer4 , 0.5 , 0 ); //* Front Lights
-const layer5 = new Layer ( backgroundLayer5 , 0.45 , 0 ); //* Front Middle Trunks  
-const layer6 = new Layer ( backgroundLayer6 , 0.25 , 0 ); //* Back Middle Trunks
-const layer7 = new Layer ( backgroundLayer7 , 0.2 , 0 ); //* Back Lights
-const layer8 = new Layer ( backgroundLayer8 , 0.1 , 0 ); //* Back Trunks
-const layer9 = new Layer ( backgroundLayer9 , 0.05 , 0 ); //* Fog Trunks
-const layer10 = new Layer ( backgroundLayer10 , 0 , 0 ); //* Light Blue Fill
-const layer11 = new Layer ( backgroundLayer11 , 0 ,  ); //* 3 Tone Grad Fill
+const layer0 = new Layer(backgroundLayer0, 1, 15); //* Ground Front
+const layer1 = new Layer(backgroundLayer1, 0.85, 0); //* Ground Back
+const layer2 = new Layer(backgroundLayer2, 0.8, 0); //* Tree Tops
+const layer3 = new Layer(backgroundLayer3, 0.7, 0); //* Front Trunks
+const layer4 = new Layer(backgroundLayer4, 0.5, 0); //* Front Lights
+const layer5 = new Layer(backgroundLayer5, 0.45, 0); //* Front Middle Trunks
+const layer6 = new Layer(backgroundLayer6, 0.25, 0); //* Back Middle Trunks
+const layer7 = new Layer(backgroundLayer7, 0.2, 0); //* Back Lights
+const layer8 = new Layer(backgroundLayer8, 0.1, 0); //* Back Trunks
+const layer9 = new Layer(backgroundLayer9, 0.05, 0); //* Fog Trunks
+const layer10 = new Layer(backgroundLayer10, 0, 0); //* Light Blue Fill
+const layer11 = new Layer(backgroundLayer11, 0); //* 3 Tone Grad Fill
 
-const gameObjects = [layer11,layer10,layer9,layer8,layer7,layer6,layer5,layer4,layer3,layer2,layer1,layer0]; // the order of this needs to be right to left
+const gameObjects = [
+  layer11,
+  layer10,
+  layer9,
+  layer8,
+  layer7,
+  layer6,
+  layer5,
+  layer4,
+  layer3,
+  layer2,
+  layer1,
+  layer0,
+]; // the order of this needs to be right to left
 
-function animationLoop () {
-    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    
-    // ctx.drawImage(backgroundLayer11, x , 0);
-    // ctx.drawImage(backgroundLayer11, x + CANVAS_WIDTH , 0);     
-    // ctx.drawImage(backgroundLayer10, x , 0);
-    // ctx.drawImage(backgroundLayer10, x + CANVAS_WIDTH , 0);
-    // ctx.drawImage(backgroundLayer9, x , 0);
-    // ctx.drawImage(backgroundLayer9, x + CANVAS_WIDTH , 0);
-    // ctx.drawImage(backgroundLayer8, x , 0);
-    // ctx.drawImage(backgroundLayer8, x + CANVAS_WIDTH , 0);
-    // ctx.drawImage(backgroundLayer7, x , 0);
-    // ctx.drawImage(backgroundLayer7, x + CANVAS_WIDTH , 0);
-    // ctx.drawImage(backgroundLayer6, x , 0);
-    // ctx.drawImage(backgroundLayer6, x + CANVAS_WIDTH , 0);
-    // ctx.drawImage(backgroundLayer5, x , 0);
-    // ctx.drawImage(backgroundLayer5, x + CANVAS_WIDTH , 0);
-    // ctx.drawImage(backgroundLayer4, x , 0);
-    // ctx.drawImage(backgroundLayer4, x + CANVAS_WIDTH , 0);
-    // ctx.drawImage(backgroundLayer3, x , 0);
-    // ctx.drawImage(backgroundLayer3, x + CANVAS_WIDTH , 0);
-    // ctx.drawImage(backgroundLayer2, x , 0);
-    // ctx.drawImage(backgroundLayer2, x + CANVAS_WIDTH , 0);     
-    // ctx.drawImage(backgroundLayer1, x , 0);
-    // ctx.drawImage(backgroundLayer1, x + CANVAS_WIDTH , 0);
-    // ctx.drawImage(backgroundLayer0, x , 0);
-    // ctx.drawImage(backgroundLayer0, x + CANVAS_WIDTH , 0);
+function animationLoop() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    // if( x < - CANVAS_WIDTH) x = 0;
-    // else x -= globalGameSpeed;
+  // ctx.drawImage(backgroundLayer11, x , 0);
+  // ctx.drawImage(backgroundLayer11, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer10, x , 0);
+  // ctx.drawImage(backgroundLayer10, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer9, x , 0);
+  // ctx.drawImage(backgroundLayer9, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer8, x , 0);
+  // ctx.drawImage(backgroundLayer8, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer7, x , 0);
+  // ctx.drawImage(backgroundLayer7, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer6, x , 0);
+  // ctx.drawImage(backgroundLayer6, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer5, x , 0);
+  // ctx.drawImage(backgroundLayer5, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer4, x , 0);
+  // ctx.drawImage(backgroundLayer4, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer3, x , 0);
+  // ctx.drawImage(backgroundLayer3, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer2, x , 0);
+  // ctx.drawImage(backgroundLayer2, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer1, x , 0);
+  // ctx.drawImage(backgroundLayer1, x + CANVAS_WIDTH , 0);
+  // ctx.drawImage(backgroundLayer0, x , 0);
+  // ctx.drawImage(backgroundLayer0, x + CANVAS_WIDTH , 0);
 
-    // layer1.update();
-    // layer1.draw();
-    // layer2.update();
-    // layer2.draw();
-    // layer3.update();
-    // layer3.draw();
-    //etc...
+  // if( x < - CANVAS_WIDTH) x = 0;
+  // else x -= globalGameSpeed;
 
-    gameObjects.forEach(object => { //!! calls the update and draws from each array class object!
-        object.update();
-        object.draw();
-    });
+  // layer1.update();
+  // layer1.draw();
+  // layer2.update();
+  // layer2.draw();
+  // layer3.update();
+  // layer3.draw();
+  //etc...
 
-    requestAnimationFrame(animationLoop);
+  gameObjects.forEach((object) => {
+    //!! calls the update and draws from each array class object!
+    object.update();
+    object.draw();
+  });
+
+  requestAnimationFrame(animationLoop);
 }
 animationLoop();
