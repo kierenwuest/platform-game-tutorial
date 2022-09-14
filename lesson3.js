6c06435ae773bd5d7220fb943b26ef33e76249fa
Kieren Wuest <kierenwuest@gmail.com>
2022-09-14 20:54:54 +0930

    lesson3a

--- lesson3.js
+++ lesson3.js
@@ -5,7 +5,7 @@
 const canvas = document.getElementById('third1');
 const ctx = canvas.getContext('2d');
 
-CANVAS_WIDTH = canvas.width = 928;
+CANVAS_WIDTH = canvas.width = 950;
 CANVAS_HEIGHT = canvas.height = 793;
 
 // entity1 = { // curly braces make this name an object
@@ -16,45 +16,48 @@ CANVAS_HEIGHT = canvas.height = 793;
 // } 
 //! Now turn this object into a 'factory' which is a class, which can make multiples when we call it based on the blueprint we give it
 
-const numberOfEntities = 200;
+const numberOfEntities = 60;
 const entityArray = [];
 
 //TODO Use images
-const entityImage = new Image();
-entityImage.src = 'images/BlueBird.png';
+// const entityImage = new Image();
+// entityImage.src = 'images/HummingBird.png';
+//* Now put them in the constructor to keep things neater
 let gameFrame = 0;
 
 class Entity { // classes are new version of older 'prototypes' which do the same thing.
     constructor(){
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.image = new Image();
+        this.image.src = 'images/HummingBird.png';
         // this.speed = Math.random() * 4 - 2; //add a new parameter 
                 //? Math.random() * 4 - 2; this is a format to do a random number between -2 and +2
                 //? [Math.random() * 4] this part creats a number between 0-4
                 //? [- 2] this part defines where to start from. -2 + [0-4 rand] // = [-2 rand +2] 
-        this.spriteWidth = 32;
-        this.spriteHeight = 32;
-        this.imageScale = Math.random() * 1.3 + 0.2; //* use this number to scale size   
+        this.spriteWidth = 64;
+        this.spriteHeight = 64;
+        this.imageScale = Math.random() * 1 + 0.4; //* use this number to scale size   
         this.width = this.spriteWidth * this.imageScale ; 
-        this.height = this.spriteHeight * this.imageScale ; 
+        this.height = this.spriteHeight * this.imageScale ;
+        this.x = Math.random() * (canvas.width - this.width); // this keeps them spawning within the canvas area and the brackets need to be there to work
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
-        this.flapSpeed = Math.floor(Math.random() * 3 + 2);
+        this.flapSpeed = Math.floor(Math.random() * 2.4 + 1.3);
     }
     update(){ // this is creating a method that can be used and is shared by the created objects (new) going to use this for movement in animation loop
         // this.x++; // replace this to add speed
         // this.y++;
         // this.x += this.speed; // using += https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition_assignment
         // this.y += this.speed;
-        this.x -= Math.random() * 4 - 1.5;
-        this.y -= Math.random() * 2 - 0.5;
+        this.x += Math.random() * 2 - 1;
+        this.y += Math.random() * 2 - 1;
         //TODO Animate the sprite
         //! using a ternary operator | given ? when : then ; | the simplest if else syntax
         if(gameFrame % this.flapSpeed === 0 ){ //* This says if the divisible remainder of the gameFrame is 0 then run the animation advance for every second animationLoop() - change speed by the % integer
-            this.frame >= 2 ? this.frame = 0 : this.frame++;
+            this.frame >= 1 ? this.frame = 0 : this.frame++;
         }
     }
     draw(){ //taking what we would draw int he animation loop anf d putting it in here as a method too
-        ctx.drawImage(entityImage, this.frame*this.spriteWidth , 0 , this.spriteWidth , this.spriteHeight , this.x,this.y,this.width,this.height); //! ( crop 4 , draw 4)
+        ctx.drawImage(this.image, this.frame*this.spriteWidth , 0 , this.spriteWidth , this.spriteHeight , this.x,this.y,this.width,this.height); //! ( crop 4 , draw 4)
     } 
 }
 
